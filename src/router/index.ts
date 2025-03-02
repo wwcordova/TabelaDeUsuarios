0b931ac9c20496338d0219aa960f826ecbf972a0
Wagner Cordova <wcordova38@gmail.com>
2025-03-02 11:30:43 -0300

    Configuração incial

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,15 +1,16 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import Home from "../views/Home.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: "/",
     name: "home",
-    component: HomeView,
+    component: Home,
   },
   {
-    path: "/about",
-    name: "about",
+    path: "/detail/:id",
+    name: "detail",
+    name: "datail",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
